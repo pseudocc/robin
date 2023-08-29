ab7bf31c9716086c7a47001ac9e967ac0f698bd3
pseudoc <atlas.yu@canonical.com>
2023-08-29 16:04:03 +0800

    feat(editor): unicode read and write

--- src/editor.zig
+++ src/editor.zig
@@ -1,10 +1,14 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const cursor = @import("cursor.zig");
 const clear = @import("clear.zig");
 const terminal = @import("terminal.zig");
 
 const Size = terminal.Size;
 const Writer = std.fs.File.Writer;
+
+const DEBUG = builtin.mode == .Debug;
+const QMARK = std.unicode.utf8Decode("�") catch unreachable;
 
 pub const Editor = struct {
     const BufferedWriter = std.io.BufferedWriter(1024, Writer);
@@ -31,15 +35,56 @@ pub const Editor = struct {
         return self;
     }
 
-    pub fn key(self: *Self, byte: u8) !void {
+    pub fn input(self: *Self, data: []const u8, force: bool) !usize {
+        if (DEBUG) {
+            var writer = self.buffer.writer();
+            try writer.print("data length: {d}\r\n", .{data.len});
+        }
+
+        var i: usize = 0;
+        while (i < data.len) {
+            const n = std.unicode.utf8ByteSequenceLength(data[i]) catch {
+                try self.handle_unicode(QMARK);
+                continue;
+            };
+            if (n == 1) {
+                try self.handle_ascii(data[i]);
+                i += 1;
+                continue;
+            } else if (!force and i + n > data.len) {
+                break;
+            }
+
+            const codepoint = std.unicode.utf8Decode(data[i .. i + n]) //
+            catch {
+                try self.handle_unicode(QMARK);
+                continue;
+            };
+            i += n;
+            try self.handle_unicode(codepoint);
+        }
+
+        try self.render();
+        return i;
+    }
+
+    fn handle_ascii(self: *Self, byte: u8) !void {
         var writer = self.buffer.writer();
-        if (byte == 'q') {
+        if (byte == @as(u21, 'q')) {
             return Reason.Quit;
         } else if (std.ascii.isControl(byte)) {
             try writer.print("{d}\r\n", .{byte});
         } else {
             try writer.print("{d} ({c})\r\n", .{ byte, byte });
         }
+    }
+
+    fn handle_unicode(self: *Self, code: u21) !void {
+        const N = 4;
+        var bytes: [N]u8 = [_]u8{0} ** N;
+        var writer = self.buffer.writer();
+        const n = try std.unicode.utf8Encode(code, &bytes);
+        try writer.print("{s} ({d})\r\n", .{ bytes[0..@intCast(n)], code });
     }
 
     fn clear_screen(self: *Self) !void {
@@ -51,7 +96,7 @@ pub const Editor = struct {
         try self.buffer.flush();
     }
 
-    pub fn render(self: *Self) !void {
+    fn render(self: *Self) !void {
         try self.buffer.flush();
     }
 
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const terminal = @import("terminal.zig");
 const editor = @import("editor.zig");
 
@@ -6,7 +7,10 @@ const stdin = std.io.getStdIn();
 const stdout = std.io.getStdOut().writer();
 const stderr = std.io.getStdErr().writer();
 
+const DEBUG = builtin.mode == .Debug;
+
 pub fn main() !void {
+    const BUFSIZ = if (DEBUG) 32 else 1024;
     const modes = [_]terminal.Toggles{
         .RawMode,
         .AltBuffer,
@@ -22,20 +26,28 @@ pub fn main() !void {
     var e = try editor.Editor.init(stdout, try t.size());
     defer e.deinit() catch unreachable;
 
-    var buffer: [32]u8 = undefined;
+    var buffer: [BUFSIZ]u8 = undefined;
+    var offset: usize = 0;
     main_loop: while (true) {
-        const bytes_read = try stdin.read(&buffer);
-        if (bytes_read == 0) {
+        const bytes_read = try stdin.read(buffer[offset..]);
+        const end = offset + bytes_read;
+        if (end == 0) {
             continue;
         }
 
-        for (buffer[0..bytes_read]) |byte| {
-            e.key(byte) catch {
-                break :main_loop;
-            };
-        }
+        const has_input = bytes_read != 0;
+        const bytes_processed = e.input(
+            buffer[0..end],
+            !has_input,
+        ) catch {
+            break :main_loop;
+        };
 
-        try e.render();
+        const unused = end - bytes_processed;
+        if (unused != 0) {
+            @memcpy(buffer[0..unused], buffer[bytes_processed..end]);
+        }
+        offset = unused;
     }
 
     inline for (modes) |mode| {
