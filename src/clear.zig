c882bacdefd900bbfd085ad6f5891d2504784aeb
pseudoc <atlas.yu@canonical.com>
2023-09-28 16:11:34 +0800

    enhance(clear): clear n_chars after the cursor

--- src/clear.zig
+++ src/clear.zig
@@ -2,24 +2,37 @@ const std = @import("std");
 const print = std.fmt.comptimePrint;
 
 pub const Direction = enum(u8) {
-    forward = '0',
-    backward = '1',
-    both = '2',
+    forward = 0,
+    backward = 1,
+    both = 2,
 };
 
 pub const Scope = enum(u8) {
     screen = 'J',
     line = 'K',
+    char = 'X',
 };
 
-pub inline fn default(comptime s: Scope, comptime d: Direction) []const u8 {
-    return print("\x1b[{c}{c}", .{ @intFromEnum(d), @intFromEnum(s) });
+pub fn rchar(allocator: std.mem.Allocator, n: u16) ![]const u8 {
+    return try switch (n) {
+        0, 1 => std.fmt.allocPrint(allocator, "\x1b[X", .{}),
+        else => std.fmt.allocPrint(allocator, "\x1b[{d}X", .{n}),
+    };
+}
+
+inline fn ctime(comptime s: Scope, comptime n: u16) []const u8 {
+    if (n == 0) return print("\x1b[{c}", .{@intFromEnum(s)});
+    return print("\x1b[{d}{c}", .{ n, @intFromEnum(s) });
 }
 
 pub inline fn screen(comptime d: Direction) []const u8 {
-    return default(Scope.screen, d);
+    return ctime(Scope.screen, @intFromEnum(d));
 }
 
 pub inline fn line(comptime d: Direction) []const u8 {
-    return default(Scope.line, d);
+    return ctime(Scope.line, @intFromEnum(d));
+}
+
+pub inline fn char(comptime n: u16) []const u8 {
+    return ctime(Scope.char, n);
 }
